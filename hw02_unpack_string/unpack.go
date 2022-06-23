64df94d56b94f6159fdea6337ab5b8d3a7e396ce
Alexey Elagin <me@sh1kel.com>
2022-06-23 18:27:10 +0300

    HW2 is complete

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -7,35 +7,39 @@ import (
 	"unicode"
 )
 
-var ErrInvalidString = errors.New("invalid string")
-var ErrFirstSpace = errors.New("first space")
+var (
+	ErrInvalidString = errors.New("invalid string")
+	ErrFirstSpace    = errors.New("first space")
+)
 
 func Unpack(packedString string) (string, error) {
 	outString := strings.Builder{}
 	var currentSymbol rune
 	var previousSymbol rune
 	var repeat int
-	var strLen = len([]rune(packedString))
+	strLen := len([]rune(packedString))
 	var err error
 
 	for i, r := range packedString {
 		// fmt.Printf("%d Processing %c\n", i, r)
-		if i == 0 && unicode.IsDigit(r) { // Первый символ - цифра, выходим
-			return "", ErrInvalidString
+		if i == 0 {
+			switch {
+			case unicode.IsDigit(r):
+				return "", ErrInvalidString
+			case r == ' ':
+				return "", ErrFirstSpace
+			case unicode.IsLetter(r):
+				currentSymbol = r
+			}
 		}
-		if i == 0 && r == ' ' { // Первый символ - пробел, выходим
-			return "", ErrFirstSpace
-		}
-		if i == 0 && !unicode.IsDigit(r) { // Первый символ - не цифра
-			currentSymbol = r
-		}
+
 		if i > 0 {
 			previousSymbol = currentSymbol
 			currentSymbol = r
 			switch {
 			case unicode.IsDigit(currentSymbol) && unicode.IsDigit(previousSymbol): // две цифры подряд
 				return "", ErrInvalidString
-			case unicode.IsDigit(currentSymbol): // && previousSymbol != '\\': // Текущий - цифра, предыдущий - не слэш
+			case unicode.IsDigit(currentSymbol): // Текущий - цифра
 				repeat, err = strconv.Atoi(string(r))
 				if err != nil {
 					return "", err
@@ -45,16 +49,16 @@ func Unpack(packedString string) (string, error) {
 					// fmt.Printf("%d Save %c\n", i, previousSymbol)
 					outString.WriteRune(previousSymbol)
 				}
-			case !unicode.IsDigit(currentSymbol) && !unicode.IsDigit(previousSymbol):
-				// текущий не цифра, предыдыущий не цифра
+			case unicode.IsLetter(currentSymbol) && unicode.IsLetter(previousSymbol):
+				// текущий буква, предыдыущий буква
 				// fmt.Printf("%d Save %c\n", i, previousSymbol)
 				outString.WriteRune(previousSymbol)
 				if i == strLen-1 {
 					// fmt.Printf("%d Save %c\n", i, currentSymbol)
 					outString.WriteRune(currentSymbol)
 				}
-			case !unicode.IsDigit(currentSymbol) && unicode.IsDigit(previousSymbol):
-				// текущий не цифра, предыдущий - цифра
+			case unicode.IsLetter(currentSymbol) && unicode.IsDigit(previousSymbol):
+				// текущий буква, предыдущий - цифра
 				if i == strLen-1 {
 					// fmt.Printf("%d Save %c\n", i, currentSymbol)
 					outString.WriteRune(currentSymbol)
